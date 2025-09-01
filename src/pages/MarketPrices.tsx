0d2fb6f51dc180ba366eb555ab586cafac7a9c4d
Abhinav21110 <abhinavgovind43@gmail.com>
2025-09-01 20:29:30 +0530

    market prices added

--- src/pages/MarketPrices.tsx
+++ src/pages/MarketPrices.tsx
@@ -5,7 +5,7 @@ import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
-import { marketPrices } from "../data/sampleData";
+import { marketPrices } from "../data/sampleData.ts";
 
 export default function MarketPrices() {
   const [searchTerm, setSearchTerm] = useState("");
