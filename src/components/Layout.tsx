7b8d4fdf554796500f77f769b2961a2f66144aa1
Abhinav21110 <abhinavgovind43@gmail.com>
2025-09-06 08:22:02 +0530

    background image added

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -7,7 +7,7 @@ interface LayoutProps {
 
 export function Layout({ children }: LayoutProps) {
   return (
-    <div className="min-h-screen bg-background">
+    <div className="min-h-screen">
       <Navigation />
       <main className="container mx-auto px-4 py-8 pb-24 md:pb-8">
         {children}
